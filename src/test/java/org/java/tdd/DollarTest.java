e3736fbb02835d77c817dacf2c0e008b6f535d27
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-06-13 02:13:29 -0500

    Added initial code

---
+++ src/test/java/org/java/tdd/DollarTest.java
@@ -0,0 +1,27 @@
+package org.java.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mgupta on 6/13/16.
+ */
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+}
