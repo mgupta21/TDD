a94787df19126fe3b6e68b760225f05d8ca8e88d
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-06-27 03:13:47 -0500

    Refactored code

---
+++ src/test/java/org/java/tdd/MoneyTest.java
@@ -0,0 +1,35 @@
+package org.java.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mgupta on 6/13/16.
+ */
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(6).equals(Money.dollar(6)));
+    }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+
+}
